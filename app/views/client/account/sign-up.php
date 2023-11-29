6b49cd42e8a1e6719abcc954f3bbb862abae7576
NhatHa <ha.ngnhnht@gmail.com>
2023-11-29 15:48:23 +0700

    hehe quen add footer

---
+++ app/views/client/account/sign-up.php
@@ -0,0 +1,35 @@
+<div class="container center">
+    <div class="row">
+        <div class="col-md-12" id="register">
+            <h2>Tao tai khoan</h2>
+            <form action="" method="POST">
+                <div class="form-group">
+                    <label for="name">Họ và Tên</label>
+                    <input type="text" class="form-control" name="name" id="name">
+                </div>
+                <div class="form-group">
+                    <label for="phone">Số điện thoại</label>
+                    <input type="text" class="form-control" name="phone" id="phone">
+                </div>
+                <div class="form-group">
+                    <label for="email">E-mail</label>
+                    <input type="text" class="form-control" name="email" id="email">
+                </div>
+                <div class="form-group">
+                    <label for="password">Mật khẩu</label>
+                    <input type="password" class="form-control" name="password" id="password">
+                </div>
+                <div class="form-group">
+                    <label for="confirm-password">Xác nhận mật khẩu</label>
+                    <input type="password" class="form-control" name="confirm-password" id="confirm-password">
+                </div>
+                <div>
+                    <input type="checkbox" name="agree" id="agree">
+                    <label for="agree">Khách hàng đồng ý cung cấp Thông Tin Cá Nhân và cho phép The Pizza Company sử dụng Thông Tin Cá Nhân phù hợp với Chính sách bảo mật này.</label>
+                    <a href="#">Xem chi tiết</a>
+                </div>
+                <input type="submit" value="Đăng ký" class="btn btn-primary">
+            </form>
+        </div>
+    </div>
+</div>
---
+++ app/views/components/footer.php
@@ -0,0 +1,66 @@
+<footer class="page-footer">
+    <div class="page-footer__top">
+        <div class="container">
+          <!-- <div class="page-footer__logo">
+            <div class="logo">
+              <a class="logo__link" href="http://localhost/assignment/Home/index"><img class="logo__img" src="../../../assignment/public/assets/img/logococoon.png" alt="logo"></a>
+            </div>
+          </div> -->
+
+          <div class="page-footer__sections">
+            <div class="contact-item-box">
+              <div class="logo">
+                <a class="logo__link" href="http://localhost/assignment/Home/index"><img class="logo__img" src="../../../assignment/public/assets/img/logococoon.png" alt="logo"></a>
+              </div>
+            </div>
+            <div class="contact-item-box">
+              <div class="contact-item-box__title">GIỚI THIỆU</div>
+              <div class="contact-item-box__link"><a href="#">Hệ thống nhà hàng</a></div>
+              <div class="contact-item-box__link"><a href="#">Câu chuyện thương hiệu</a></div>
+              <div class="contact-item-box__link"><a href="#">Ưu đãi thành viên</a></div>
+              <div class="contact-item-box__link"><a href="#">Tin tức & sự kiện</a></div>
+              <div class="contact-item-box__link"><a href="#">Tuyển dụng</a></div>
+
+              <div class="contact-item-box__title">VĂN PHÒNG ĐẠI DIỆN</div>
+              <div class="contact-item-box__value">
+                Công ty Cổ phần Pizza Ngon <br>
+                77 Trần Nhân Tôn, Phường 9, <br> Quận 5, Thành phố Hồ Chí Minh <br>
+                SĐT: +84 (028) 7308 3377 <br>
+                MST: 0104115527 <br>
+                Cấp lần đầu ngày 17 tháng 08 năm <br> 2009 và có thể được sửa đổi vào <br> từng thời điểm <br>
+              </div>
+            </div>
+
+            <div class="contact-item-box">
+              <div class="contact-item-box__title">LIÊN HỆ</div>
+              <div class="contact-item-box__link"><a href="#">Liên hệ</a></div>
+              <div class="contact-item-box__link"><a href="#">Hướng dẫn mua hàng</a></div>
+              <div class="contact-item-box__link"><a href="#">Chính sách giao hàng</a></div>
+              <div class="contact-item-box__link"><a href="#">Chính sách bảo mật</a></div>
+              <div class="contact-item-box__link"><a href="#">Điều khoản và Điều kiện</a></div>
+              
+              <div class="contact-item-box__title">TỔNG ĐÀI HỖ TRỢ</div>
+              <div class="contact-item-box__link"><a href="#">Đặt hàng: 1900 6066 (9:30 – 21:30)</a></div>
+              <div class="contact-item-box__link"><a href="#">Tổng đài CSKH: 1900 633 606 (9:30 - 21:30)</a></div>
+            </div>
+
+            <div class="contact-item-box">
+              <div class="contact-item-box__title">LIÊN KẾT VỚI CHÚNG TÔI</div>
+              <div class="page-footer__social">
+                <ul class="social">
+                  <li class="social-item"><a class="social-link" href="#!"><i class="fab fa-facebook-f"></i></a></li>
+                  <li class="social-item"><a class="social-link" href="#!"><i class="fab fa-instagram"></i></a></li>
+                  <li class="social-item"><a class="social-link" href="#!"><i class="fab fa-youtube"></i></a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="page-footer__bottom">
+        <div class="container">
+          <div class="page-footer__copy"><span>Bản quyền © 2023 The Pizza Company. Đã đăng ký bản quyền.</div>
+        </div>
+      </div> 
+</footer>
