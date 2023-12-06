c28825c2a3dc9aa1a8498a5deae0bc0ff91209c8
dotiennee <149573030+dotiennee@users.noreply.github.com>
2023-12-06 11:50:39 +0700

    update

--- app/views/client/homepage/popup_productdetail.php
+++ app/views/client/homepage/popup_productdetail.php
@@ -1,7 +1,7 @@
 
 <div class="overplay">
     <div class="popup_productdetail">
-        <div class="popupproductleft col-6">
+        <div class="popupproductleft col-sm-6">
             <div class="popup_productimage">
                 <img src="https://thepizzacompany.vn/images/thumbs/000/0003791_caramelized-french-onion-cheese-tart_500.jpeg" class="productimage"  alt="Ảnh của product"></img>
             </div>
@@ -9,7 +9,7 @@
                 79000đ
             </div>
         </div>
-        <div class="popupright_product col-6">
+        <div class="popupright_product col-sm-6">
             <div class="popup_infor">
                 <div class="popup_productname">
                     <h3 class="popup_name">
