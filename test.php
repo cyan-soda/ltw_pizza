90a84697c0eb85e8ea925b3984a96f4910a54c01
dotiennee <149573030+dotiennee@users.noreply.github.com>
2023-12-06 14:05:24 +0700

    response orderlist-productdetail-carousel

--- test.php
+++ test.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="public/css/header.css" />
     <link rel="stylesheet" href="public/css/footer.css" />
-    <link rel="stylesheet" href="public/css/orderlist.css" />
-    <link rel="stylesheet" href="public/css/carousel.css" />
+    <link rel="stylesheet" href="public/css/orderlist.css?v=<?php echo time(); ?>" />
+    <link rel="stylesheet" href="public/css/carousel.css?v=<?php echo time(); ?>" />
     <link rel="stylesheet" href="public/css/popup_orderlist.css" />
     <link rel="stylesheet" href="public/css/menu.css" />
     <link rel="stylesheet" href="public/css/payment.css" />
@@ -20,12 +20,18 @@
 </head>
 
 <body>
+<<<<<<< HEAD
   <!-- <?php include '../ltw_pizza/app/views/components/header.php' ?>  -->
   <!-- <div>
   <?php include '../ltw_pizza/app/views/components/menu_grid.php' ?>
   </div> -->
   <?php include '../ltw_pizza/app/views/client/payment/payment_delivery.php' ?>
   <?php include '../ltw_pizza/app/views/components/footer.php' ?>
+=======
+  
+  
+  <?php include '../ltw_pizza/app/views/client/homepage/carousel.php' ?>
+>>>>>>> 169227d (response orderlist-productdetail-carousel)
 
 </body>
 
